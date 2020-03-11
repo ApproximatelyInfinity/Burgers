09b60637733b5efe63579a048948b29162821df4
ApproximatelyInfinity <dbrogdon91@gmail.com>
2020-03-11 02:29:04 -0500

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+function burger() {
+  document.getElementById("btn-menu").classList.toggle("is-active");
+  document.getElementById("btn-menu").classList.toggle("btn-active");
+}
